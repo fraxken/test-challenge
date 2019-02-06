e5e1da758afae08aca415ee518bdb7b63fef5a86
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-02-07 00:14:20 +0100

    refactor: add jsdoc to 02.js

--- 02.js
+++ 02.js
@@ -1,4 +1,27 @@
+/**
+ * @typedef {Object} VectorLike
+ * @property {Number} x
+ * @property {Number} y
+ * @property {Number} z
+ */
+
+/**
+ * @class Vector
+ * 
+ * @property {Number} x
+ * @property {Number} y
+ * @property {Number} z
+ */
 class Vector {
+    /**
+     * @constructor
+     * @memberof Vector#
+     * @param {Number} [x=0] x
+     * @param {Number} [y=0] y
+     * @param {Number} [z=0] z
+     *
+     * @throws {TypeError}
+     */
     constructor(x = 0, y = 0, z = 0) {
         if (typeof x !== "number") {
             throw new TypeError("x must be a number");
@@ -15,6 +38,14 @@ class Vector {
         this.z = z;
     }
 
+    /**
+     * @method add
+     * @memberof Vector#
+     * @param {!Vector} vec vector Object
+     * @returns {void}
+     *
+     * @throws {TypeError}
+     */
     add(vec) {
         if (!(vec instanceof Vector)) {
             throw new TypeError("vec must be an instanceof Vector");
@@ -25,6 +56,11 @@ class Vector {
         this.z += vec.z;
     }
 
+    /**
+     * @method toJSON
+     * @memberof Vector#
+     * @returns {VectorLike}
+     */
     toJSON() {
         return { x: this.x, y: this.y, z: this.z };
     }
