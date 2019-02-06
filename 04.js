20f8e5b8dbb25ebb3f49216b3e61cbb040428f9c
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-02-07 00:20:12 +0100

    refactor: add jsdco

--- 04.js
+++ 04.js
@@ -3,8 +3,18 @@ const cases = {
     mul: (a, b) => a * b,
     sub: (a, b) => a - b
 }
+
+/** @type {Set<String>} */
 const availableActions = new Set(Object.keys(cases));
 
+/**
+ * @func calc
+ * @param {!String} actionName actionName
+ * @param  {...any} args args
+ * @returns {Number}
+ * 
+ * @throws {Error}
+ */
 function calc(actionName, ...args) {
     if (!availableActions.has(actionName)) {
         throw new Error(`Unknow action '${actionName}'`);
