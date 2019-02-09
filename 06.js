598b2ae8d488b61d5bdb9d9035712476bdbb9e3a
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-02-10 00:06:38 +0100

    feat: add 06 (proxy)

---
+++ 06.js
@@ -0,0 +1,23 @@
+const oP = {
+    foo: 10
+};
+
+const handler = {
+    get(obj, prop) {
+        if (!Reflect.has(obj, prop)) {
+            throw new Error(`Unknown property ${prop} in target object!`);
+        }
+
+        return Reflect.get(obj, prop) ** 2;
+    },
+    set(obj, prop, value) {
+        if (typeof value !== "number") {
+            throw new TypeError("value must be a number");
+        }
+
+        Reflect.set(obj, prop, value);
+        return true;
+    }
+}
+
+module.exports = new Proxy(oP, handler);
